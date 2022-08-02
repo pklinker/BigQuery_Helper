291a6d6f35444b236720f836e86aafe75bec016a
Paul Klinker <pklinker@gmail.com>
2022-08-02 09:42:28 -0400

    Update setup.py
    Change name

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from version import __version__ as version
 
 
-setup(name='bq_helper',
+setup(name='BigQuery_Helper',
       version=version,
       description='Helper class to simplify common read-only BigQuery tasks.',
       author='Sohier Dane',
-      url='https://github.com/SohierDane/BigQuery_Helper',
+      url='https://github.com/pklinker/BigQuery_Helper',
       license='Apache 2.0',
       install_requires=['pandas', 'google-cloud-bigquery'],
       classifiers=['Programming Language :: Python :: 3'],
