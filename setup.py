0fb71fbfd8eeda71fb116445193468eaa2be53a1
Paul Klinker <pklinker@gmail.com>
2022-08-02 09:11:24 -0400

    Update setup.py
    Fix build failure in jupyter notebook doing pip install: explicitly set py_modules with a list of names

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(name='bq_helper',
       license='Apache 2.0',
       install_requires=['pandas', 'google-cloud-bigquery'],
       classifiers=['Programming Language :: Python :: 3'],
+      py_modules=[],
       )
